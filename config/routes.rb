fffd3ae8b9deb8057a72b417a670293bf385f1b3
Kacper Nędzyński <kacper.nedzynski10@gmail.com>
2025-09-15 15:14:23 +0200

    Add article creation feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create]
 
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
