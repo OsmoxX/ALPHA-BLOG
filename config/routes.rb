d28336cb10e2c33d3447ce9bcfdf20b50eb07042
Kacper Nędzyński <kacper.nedzynski10@gmail.com>
2025-10-02 20:53:39 +0200

    Added a show and delete methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles
 
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
