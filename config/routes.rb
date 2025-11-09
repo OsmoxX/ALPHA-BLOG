31f4e82bd49298bad6586d2fc9d44eba8075f64a
Kacper Nędzyński <kacper.nedzynski10@gmail.com>
2025-11-10 00:01:52 +0100

    Add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
 
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
