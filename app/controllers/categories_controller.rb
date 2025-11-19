2a8ec49e37b56ece4b4f827884b95783abd07e5d
Kacper Nędzyński <kacper.nedzynski10@gmail.com>
2025-11-19 20:14:40 +0100

    feat: Add views for creating categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,10 +3,28 @@ class CategoriesController < ApplicationController
   def index
 
   end
-  def new
 
+  def new
+    @category = Category.new
   end
+
   def show
 
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created."
+      redirect_to @category
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
